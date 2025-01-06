d86ec06e9403773dd77f19f82f2c6712cd82d122
Zain Mushtaq <zain.mushtaq@fmsystem.org>
2025-01-06 15:35:04 +0500

    allow-----

--- auth-check.js
+++ auth-check.js
@@ -1,13 +1,13 @@
 (function() {
     const allowedMRNOs = ['10060000005622', '10060000002293','10060000000579','10060000001970','10060000002301'];
-    const adminMRNO = 'admin';
+
 
     function checkAuth() {
         const isLoggedIn = localStorage.getItem('isLoggedIn');
         const mrno = localStorage.getItem('mrno');
-        const isAdmin = localStorage.getItem('isAdmin') === 'true';
 
-        if (!isLoggedIn || isLoggedIn !== 'true' || (!allowedMRNOs.includes(mrno) && !isAdmin)) {
+
+        if (!isLoggedIn || isLoggedIn !== 'true' || (!allowedMRNOs.includes(mrno)) ){
             window.location.href = 'index.html';
         }
     }
