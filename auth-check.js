20527d8305b0bc017dbd2a5fabc94627ec5c9ca2
Zain Mushtaq <zain.mushtaq@fmsystem.org>
2025-01-06 15:52:27 +0500

    allow-------

--- auth-check.js
+++ auth-check.js
@@ -1,21 +1,23 @@
 (function() {
-    const allowedMRNOs = ['10060000005622', '10060000002293','10060000000579','10060000001970','10060000002301'];
-
+    const allowedMRNOs = ['10060000005622', '10060000002293', '10060000000579','10060000001970','10060000002301'];
 
     function checkAuth() {
-        const isLoggedIn = localStorage.getItem('isLoggedIn');
+        const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
         const mrno = localStorage.getItem('mrno');
 
-
-        if (!isLoggedIn || isLoggedIn !== 'true' || (!allowedMRNOs.includes(mrno)) ){
+        // Redirect to login if not logged in or if MRNO is not allowed
+        if (!isLoggedIn || !allowedMRNOs.includes(mrno)) {
             window.location.href = 'index.html';
         }
     }
+
+    // Run the check immediately
     checkAuth();
 
+    // Also run the check when the page is shown (in case of browser back button use)
     document.addEventListener('visibilitychange', function() {
-      if (!document.hidden) {
-        checkAuth();
-      }
+        if (!document.hidden) {
+            checkAuth();
+        }
     });
-  })();
+})();
